5262b9309ac143cb1353e3def27f8043d4e6d420
mryrgm <maryruth.argame.iics@ust.edu.ph>
2018-09-22 12:59:43 +0800

    Initial commit

---
+++ app/src/main/java/com/argame/mary/LabActivity3.kt
@@ -0,0 +1,12 @@
+package com.argame.mary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LabActivity3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab3)
+    }
+}
